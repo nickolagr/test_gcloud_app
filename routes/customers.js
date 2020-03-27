c4151afc3d8db2dcb90d29b6625fdc19bd721616
nickolagr <mgrechanyk@rambler.ru>
2020-03-27 19:13:01 +0200

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -12,7 +12,7 @@ const connect = () => {
   config.host = `/cloudsql/northern-music-270914:europe-west1:app-db`;
 
   // Establish a connection to the database
-  const knex = Knex({
+  const knex = require("knex")({
     client: 'pg',
     connection: config,
   });
