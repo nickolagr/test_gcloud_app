5f033b4cc6d38f4548524bed90321bf77fdea740
nickolagr <mhrechaniuk@ukr.net>
2020-05-06 12:49:26 +0300

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sudo systemctl stop nginx
+sudo systemctl stop nginx
 cd /home/ubuntu
 # check if folder exists and remove it if so
-#if sudo [ -d "www" ]; then sudo rm -Rf www; fi
+if sudo [ -d "www" ]; then sudo rm -Rf www; fi
